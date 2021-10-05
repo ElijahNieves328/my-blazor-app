b1c6b4c8fc5585f07a511e8a828777bbdba454b7
Elijah Nieves <ElijahNieves328@gmail.com>
2021-10-04 22:15:47 -0400

    Added a Todo List, changed NavMenu icons, changed website icon

---
+++ Client/TodoItem.cs
@@ -0,0 +1,5 @@
+public class TodoItem       //creates a class for each todo object to use
+{
+    public string Title { get; set; }   //creates a string property to get/set the task name
+    public bool IsDone { get; set; }    //creates a boolean property to get/set if the task is done or not
+}
